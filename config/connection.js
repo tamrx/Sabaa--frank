1223943bf2436aa288e448df8b44bd59d8a227f9
tawfeekamr91 <tawfeek@realblocks>
2022-04-29 02:30:36 +0400

    First commit

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,6 @@
 // import sequelize
+const dbConfig = require("../config/db.config.js");
+
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
@@ -8,11 +10,17 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-  });
+     sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+        host: dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        operatorsAliases: false,
+        pool: {
+          max: dbConfig.pool.max,
+          min: dbConfig.pool.min,
+          acquire: dbConfig.pool.acquire,
+          idle: dbConfig.pool.idle
+        }
+      });
 }
 
 module.exports = sequelize;
